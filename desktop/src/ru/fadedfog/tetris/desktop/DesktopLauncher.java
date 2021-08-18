ee0231a329b3326de407ce2e167fe201a8a16655
Ilya Bastrakov <i.bastrakov.00@gmail.com>
2021-08-19 00:06:25 +0300

    first commit

---
+++ desktop/src/ru/fadedfog/tetris/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.fadedfog.tetris.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.fadedfog.tetris.TetrisGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TetrisGame(), config);
+	}
+}
